de61a23a26fb336f503d21d78ddf6b6be645b97d
meganezarukamo <chuantianhuitai19@gmail.com>
2024-06-20 12:18:50 +0900

    dorixyoku

--- work4.py
+++ work4.py
@@ -1,4 +1,6 @@
 import tkinter as tk
+import random
+
 
 # ↓↓↓ お約束のコード ↓↓↓
 window = tk.Tk()
@@ -15,10 +17,14 @@ text1.pack(pady=10)
 name_list = []
 
 
-def button_action():  # 関数の定義 ※ボタンが押されたときの動き
+def button_action1():  # 関数の定義 ※ボタンが押されたときの動き
     user_input = entry1.get()  # 入力値を取得
     name_list.append(user_input)
     label1.config(text="\n".join(name_list))  # 画面に出力
+
+
+def button_action2():  # 関数の定義 ※ボタンが押されたときの動き
+    label2.config(text=random.choice(name_list))
 
 
 # 入力フィールドの作成
@@ -26,12 +32,18 @@ entry1 = tk.Entry(window, bg=fg_color, fg=bg_color)
 entry1.pack(pady=10)
 
 # ボタンの作成
-button1 = tk.Button(window, text="新しいお友達", command=button_action)
+button1 = tk.Button(window, text="新しいお友達", command=button_action1)
 button1.pack(pady=10)
 
 # 出力ラベルの作成
 label1 = tk.Label(window, text="", bg=bg_color, fg=fg_color)
 label1.pack(pady=10)
+
+button2 = tk.Button(window, text="選ぶよ", command=button_action2)
+button2.pack(pady=10)
+
+label2 = tk.Label(window, text="", bg=bg_color, fg=fg_color)
+label2.pack(pady=10)
 
 # ↓↓↓ お約束のコード ↓↓↓
 window.mainloop()
